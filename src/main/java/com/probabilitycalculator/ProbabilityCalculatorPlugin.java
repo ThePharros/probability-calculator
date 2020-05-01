c73219115116bab8519a698a420c3b510c66b3cc
Pharros <thepharros@gmail.com>
2020-05-01 16:57:38 -0400

    update

--- src/main/java/com/probabilitycalculator/ProbabilityCalculatorPlugin.java
+++ src/main/java/com/probabilitycalculator/ProbabilityCalculatorPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.probabilitycalculator;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Probability Calculator"
 )
-public class ExamplePlugin extends Plugin
+public class ProbabilityCalculatorPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private ProbabilityCalculatorConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	ProbabilityCalculatorConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(ProbabilityCalculatorConfig.class);
 	}
 }
--- src/test/java/com/probabilitycalculator/ProbabilityCalculatorPluginTest.java
+++ src/test/java/com/probabilitycalculator/ProbabilityCalculatorPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.probabilitycalculator;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class ProbabilityCalculatorPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(ProbabilityCalculatorPlugin.class);
 		RuneLite.main(args);
 	}
 }
